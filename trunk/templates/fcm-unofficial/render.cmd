f5562bc008da7e6ac2687b89972bae7e0e66988c
dweiss <dweiss@7639d1ca-a90c-0410-8f03-f18e5cda5bb5>
2006-06-20 12:25:13 +0000

    Mysle, ze template jest gotowy.
    git-svn-id: https://ophelia.cs.put.poznan.pl/svn/put-latex@12 7639d1ca-a90c-0410-8f03-f18e5cda5bb5

---
+++ trunk/templates/fcm-unofficial/render.cmd
@@ -0,0 +1,13 @@
+@echo off
+
+pdflatex thesis-english.tex 
+bibtex thesis-english
+pdflatex thesis-english.tex 
+pdflatex thesis-english.tex 
+
+pdflatex thesis-polski.tex 
+bibtex thesis-polski
+pdflatex thesis-polski.tex 
+pdflatex thesis-polski.tex 
+
+del *.aux *.bak *.log *.blg *.bbl *.toc *.out
