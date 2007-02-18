1103ad31fc2d3f5215aec38a36eb561327fd5a95
dweiss <dweiss@7639d1ca-a90c-0410-8f03-f18e5cda5bb5>
2007-02-18 21:29:55 +0000

    - Update to the thesis style to allow rendering of bachelor's theses. Previously it was impossible to change the title page's header and give more than one author. Now these options are possible -- examples are given.
    git-svn-id: https://ophelia.cs.put.poznan.pl/svn/put-latex@42 7639d1ca-a90c-0410-8f03-f18e5cda5bb5

--- trunk/templates/fcm-unofficial/render.cmd
+++ trunk/templates/fcm-unofficial/render.cmd
@@ -1,13 +1,24 @@
 @echo off
 
-pdflatex thesis-english.tex 
-bibtex thesis-english
-pdflatex thesis-english.tex 
-pdflatex thesis-english.tex 
+pdflatex thesis-bachelor-english.tex 
+bibtex   thesis-bachelor-english
+pdflatex thesis-bachelor-english.tex 
+pdflatex thesis-bachelor-english.tex 
 
-pdflatex thesis-polski.tex 
-bibtex thesis-polski
-pdflatex thesis-polski.tex 
-pdflatex thesis-polski.tex 
+pdflatex thesis-master-english.tex 
+bibtex   thesis-master-english
+pdflatex thesis-master-english.tex 
+pdflatex thesis-master-english.tex 
+
+pdflatex thesis-bachelor-polski.tex 
+bibtex   thesis-bachelor-polski
+pdflatex thesis-bachelor-polski.tex 
+pdflatex thesis-bachelor-polski.tex 
+
+pdflatex thesis-master-polski.tex 
+bibtex   thesis-master-polski
+pdflatex thesis-master-polski.tex 
+pdflatex thesis-master-polski.tex 
+
 
 del *.aux *.bak *.log *.blg *.bbl *.toc *.out
