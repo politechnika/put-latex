49b750a2909923d6d6e9a46af4300264e92f6efe
dweiss <dweiss@7639d1ca-a90c-0410-8f03-f18e5cda5bb5>
2007-02-18 22:09:02 +0000

    - Added album numbers to bachelor thesis authors. - Modified thinkubator example to use the current template. - Corrected BibTeX entries in the thinkubator example.
    git-svn-id: https://ophelia.cs.put.poznan.pl/svn/put-latex@44 7639d1ca-a90c-0410-8f03-f18e5cda5bb5

--- trunk/templates/example-thinkubator/render.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-pdfplatex thesis-polski.tex || pdflatex thesis-polski.tex
-bibtex thesis-polski
-pdfplatex thesis-polski.tex || pdflatex thesis-polski.tex
-pdfplatex thesis-polski.tex || pdflatex thesis-polski.tex
-
-rm -f *.aux *.bak *.log *.blg *.bbl *.toc *.out
