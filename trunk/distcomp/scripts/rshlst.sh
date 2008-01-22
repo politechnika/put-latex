c0d4174b329ca73a1f47d6de60a217676c48b826
pwesolek <pwesolek@7639d1ca-a90c-0410-8f03-f18e5cda5bb5>
2008-01-22 11:24:51 +0000

    Added possibility to specify ssh username.
    git-svn-id: https://ophelia.cs.put.poznan.pl/svn/put-latex@68 7639d1ca-a90c-0410-8f03-f18e5cda5bb5

--- trunk/distcomp/scripts/rshlst.sh
+++ trunk/distcomp/scripts/rshlst.sh
@@ -2,12 +2,18 @@
 
 if [ $# -lt 1 ]; then
   echo "Lists SSH-accessible hosts. Usage:"
-  echo "$0 hosts_file"
+  echo "$0 hosts_file [username]"
   exit 1
 fi
 
 HOSTS=$1
 shift
+
+if [ $# -lt 1 ]; then
+  SSH_USER=$USER
+else
+  SSH_USER=$1
+fi
 
 # Don't wait too long for inaccessible hosts.
 SSH_OPTS="-o ConnectTimeout=2 -o BatchMode=yes"
@@ -17,7 +23,7 @@ sed -e 's/#.*//' -e '/^[ \t]*$/d' $HOSTS > $TMP/hosts
 
 for H in `cat $TMP/hosts`
 do
-  (ssh -q $SSH_OPTS $H "true" && echo $H ) > $TMP/$H.result &
+  (ssh -q $SSH_OPTS -l $SSH_USER $H "true" && echo $H ) > $TMP/$H.result &
 done
 
 wait
